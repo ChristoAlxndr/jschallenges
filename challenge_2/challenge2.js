816e9f5e4717dd10fe8af304d82e12ce806ed0d4
Christopher Alexander <ChristoAlxndr@Gmail.com>
2016-09-13 15:56:03 -0400

    Challenge2 completed 09/13/2016 @ 15:56

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,26 @@
 $(function(){
 
-	//all your code should go here
+var replaced = $("body").html().replace(/Bacon/g,'LASER VISION');
+$("body").html(replaced);
 
-})
+var state = true;
+   $( "h1" ).on("click", function() {
+     if ( state ) {       
+        $("h1").css({color: "#ff0000"});
+      } else {
+        $("h1").css({color: "#000000"});
+     }
+     state = !state;
+   });
+
+$('.title:even').hide();
+$('footer li:even').hide();
+
+$('.post:last').remove();
+$('.post:last').remove();
+$('.post:last').remove();
+
+$('aside').remove();
+
+
+});
